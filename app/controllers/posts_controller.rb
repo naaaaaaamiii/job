155997bf29c3155a94b53ac6dd14f6190b9a50ad
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-05 08:17:11 +0000

    user controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-   # @post = Post.find(params[:id])
   end
   
   def show
+     @post = Post.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
  
   resources :posts, only: [:index, :show, :edit, :create, :update, :destroy, :new]
- 
+  resources :users, only: [:index, :show, :edit, :update]
  
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
