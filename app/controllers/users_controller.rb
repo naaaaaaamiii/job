fb8a41e27d914a93e4b57323bb9b9546f11052ff
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-05 13:17:48 +0000

    user edit error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
   def index
     
   end
 
   def show
-    
   end
 
   def edit
-    
+
   end
+  
+ 
+  private
+   def user_params
+    params.require(:user).permit(:name, :introduction)
+   end
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :posts
+  has_one_attached :image
+  
+   def get_image(width, height)
+    unless post_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.png')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
   
 end
