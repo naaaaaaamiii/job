73e60fe146f59721e130e644771c23cfcae0f790
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-06 07:22:00 +0000

    いいねの位置

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,14 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to posts_path
+    redirect_to post_path(post)
     
  end
   
   def destroy
     podt = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(podt_id: podt.id)
+    favorite = current_user.favorites.find_by(post_id: podt.id)
     favorite.destroy
-    redirect_to posts_path
+    redirect_to post_path(post)
   end
 end
