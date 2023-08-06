86562ed70001d3faf2a3818a4f43662170265334
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-06 07:57:28 +0000

    いいね非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to post_path(post)
+    #redirect_to post_path(post)
     
  end
   
@@ -11,6 +11,6 @@ class FavoritesController < ApplicationController
     podt = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: podt.id)
     favorite.destroy
-    redirect_to post_path(post)
+    #redirect_to post_path(post)
   end
 end
