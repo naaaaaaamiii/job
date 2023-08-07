c10ab83d8efbf1c4ccec49ce43298a3b26f98d85
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-07 13:50:51 +0000

    destroyだけいけた！

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,16 +5,16 @@ class PostCommentsController < ApplicationController
    comment = PostComment.new(post_comment_params)
    comment.user_id = current_user.id
    comment.post_id = @post.id
-   comment.save
+   comment.save!
    #redirect_to request.referer 非同期のためコメントアウト
    @post_comment = PostComment.new
   end
   
   def destroy
    @post_comment = PostComment.find(params[:id])
-   @post_comment.destroy
+   @post_comment.destroy!
    #redirect_to request.referer　非同期のためコメントアウト
-   @post = @post = Post.find(params[:id])
+   @post = Post.find(params[:post_id])
   end
   
   def post_comment_params
