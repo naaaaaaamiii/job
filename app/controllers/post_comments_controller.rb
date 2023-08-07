75b8ffba216eaef47a7d6454849eee1d5f25daf5
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-07 02:32:30 +0000

    コメント入力欄

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,4 +1,4 @@
-class PostCommntsController < ApplicationController
+class PostCommentsController < ApplicationController
   def create
    post = Post.find(params[:post_id])
    comment = current_user.post_comments.new(post_comment_params)
