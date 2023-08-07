88720c623a9de04172167a763cb5814c171422f6
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-07 14:09:33 +0000

    :o:

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,13 +1,13 @@
 class PostCommentsController < ApplicationController
  
   def create
+   @post_comment = PostComment.new
    @post = Post.find(params[:post_id])
    comment = PostComment.new(post_comment_params)
    comment.user_id = current_user.id
    comment.post_id = @post.id
    comment.save!
    #redirect_to request.referer 非同期のためコメントアウト
-   @post_comment = PostComment.new
   end
   
   def destroy
