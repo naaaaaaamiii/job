9309957d7eb373309525b3b8474076e4cf2e8363
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-08 02:23:51 +0000

    dekita

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,23 +1,25 @@
 class PostCommentsController < ApplicationController
- 
+
   def create
    @post = Post.find(params[:post_id])
-   @post_comment = PostComment.new(post_comment_params)
-   @post_comment.user_id = current_user.id
-   @post_comment.post_id = @post.id
-   @post_comment.save!
+   comment = PostComment.new(post_comment_params)
+   comment.user_id = current_user.id
+   comment.post_id = @post.id
+   comment.save!
   # redirect_to request.referer #非同期のためコメントアウト
+   @post_comment = PostComment.new(post_comment_params)
+   
   end
-  
+
   def destroy
    @post_comment = PostComment.find(params[:id])
    @post_comment.destroy!
    #redirect_to request.referer 非同期のためコメントアウト
    @post = Post.find(params[:post_id])
   end
-  
+
   def post_comment_params
    params.require(:post_comment).permit(:comment, :post_id)
   end
-  
+
 end
