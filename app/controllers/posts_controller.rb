7c5ee1525a7cf596bf67494837f836eeaa123467
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-09 05:58:09 +0000

    post destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,6 +41,7 @@ class PostsController < ApplicationController
   def destroy
      post = Post.find(params[:id])
      post.destroy
+     redirect_to posts_path
   end
   
   def search_tag #検索機能(タグのみ)
