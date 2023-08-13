29d80bbdab56e5786677b6ea956a731e9f8380eb
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-13 05:37:24 +0000

    relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :posts                              #記事がなくなるとサイトが成り立たないのでユーザーが消えても記事は残す
   has_many :favorites,    dependent: :destroy  #いいね
   has_many :post_comments                      #コメント ユーザーが消えてもコメントは残す
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォローした
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォローされた
  
    def self.looks(search, word) #検索方法
       @user = User.where("title LIKE?","%#{word}%")
@@ -14,12 +16,12 @@ class User < ApplicationRecord
  
   has_one_attached :image
 
-  def get_image(width, height)
+  def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.png')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image.variant(resize_to_limit: [width, height]).processed
+      image.variant(resize_to_limit: [width,height]).processed
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_11_092710) do
+ActiveRecord::Schema.define(version: 2023_08_13_053028) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -104,6 +104,13 @@ ActiveRecord::Schema.define(version: 2023_08_11_092710) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "impressions_count", default: 0
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
