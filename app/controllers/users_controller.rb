baf77cf90cde7bc2a85af1ea31d04d5d0b12ba5e
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-13 08:56:52 +0000

    edit btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to users_path
+    redirect_to request.referer
   end
   
   def favorites #いいね一覧表示のための
