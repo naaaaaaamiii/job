8db2dd04988a09a9a799f789eef20b76e0afe708
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-14 01:45:07 +0000

    follow 非同期通信化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,13 @@
 class RelationshipsController < ApplicationController
    def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    #redirect_to request.referer #非同期通信化のためコメントアウト
    end
    
    def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    @user = User.find(params[:user_id])
+    #redirect_to request.referer #非同期通信化のためコメントアウト
    end
 end
