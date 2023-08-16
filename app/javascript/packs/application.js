20b5596b83cb68e6cec76265e691d9bee2f7bed2
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-16 11:47:15 +0000

    swip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
-
+import Swiper from 'swiper/swiper-bundle.js'; 
+import 'swiper/swiper-bundle.css';
 import "jquery";
 import "popper.js";
 import "bootstrap";
