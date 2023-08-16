a78808d7457254fdfc878625d9d6e05f86b9ae9d
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-16 04:11:56 +0000

    event new

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :admin
+  
+  enum event_status: { offline: 0, online: 1 } #イベント開催オフラインかオンラインか
+  
+  has_one_attached :event_image
+  
+  def get_event_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.png')
+      event_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      event_image.variant(resize_to_limit: [width,height]).processed
+  end
 end
