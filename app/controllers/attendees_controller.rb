aee13279d847dd94df249b560291dcfa3530d696
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-19 07:51:23 +0000

    join btn

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,13 +1,13 @@
 class AttendeesController < ApplicationController
   def create
-    @attendee = current_user.attendees.new(attendee_id: params[:attendee_id])
-    attendee.save
+    @attendee = current_user.attendees.new(event_id: params[:event_id])
+    @attendee.save
     redirect_to request.referer
   end
   
   def destroy
-    @attendee = current_user.attendees.find_by(attendee_id: params[:attendee_id])
-    attendee.destroy
+    @attendee = current_user.attendees.find_by(event_id: params[:event_id])
+    @attendee.destroy
     redirect_to request.referer
   end
   
