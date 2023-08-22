1b9eb9397e49f078da03965d9f6ebca95d1e185b
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-22 14:07:09 +0000

    layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @posts = Post.all
+    @favorites = Post.find(Favorite.group(:post_id).order('count(post_id) DESC').limit(6).pluck(:post_id))
   end
-  
+
 end
