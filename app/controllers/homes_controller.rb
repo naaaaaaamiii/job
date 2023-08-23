a3433fd54569034494e3d5d90d9a6d1bbbb31237
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-23 02:21:06 +0000

    layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,6 +6,8 @@ class HomesController < ApplicationController
     @favorites = Post.find(Favorite.group(:post_id).order('count(post_id) DESC').limit(6).pluck(:post_id))
     # 閲覧数が多い順
     @impressions = Post.impressions_count
+    
+    @events = Event.all
   end
 
 end
