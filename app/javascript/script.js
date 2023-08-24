235937c7320b87f6ec776d033370c71f4adbfdbb
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-24 12:16:06 +0000

    humberger

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,17 +1,11 @@
-// 
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});
 
 // Swiperのオプションを定数化
 const opt = {
-  loop: true, 
-  pagination: { 
-    el: '.swiper-pagination', 
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
   },
-  navigation: { 
+  navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   }
@@ -27,6 +21,6 @@ $(function() {
   $('.menu-trigger').on('click', function(event) {
     $(this).toggleClass('active');
     $('#sp-menu').fadeToggle();
-    event.preventDefault();
   });
 });
+
