cbcbabd825f0311201b3377322679a1d4eb1cb84
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-26 13:58:03 +0000

    layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
  before_action :ensure_guest_user, only: [:edit]
- 
+
   def index
    @user = current_user
    @posts = @user.posts.page(params[:page]).reverse_order
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
      if @user.update(user_params)
@@ -25,27 +25,27 @@ class UsersController < ApplicationController
        render 'edit'
      end
   end
-  
+
   def favorites #いいね一覧表示のための
     @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_posts = Post.find(favorites)
   end
-  
+
   def follows #フォローした一覧
     @user = User.find(params[:id])
     @users = @user.following_users
   end
-  
+
   def followers #フォローされた一覧
     @user = User.find(params[:id])
     @user = @user.follower_users
   end
-  
+
   def confirm #下書きした記事のみ表示
     @posts = current_user.posts.draft.page(params[:page]).reverse_order
   end
-  
+
   def myevents
     if params[:event_type] == "created_events"
       @myevents = current_user.events.where(creator_id: current_user.id)
@@ -55,19 +55,19 @@ class UsersController < ApplicationController
       @myevents = Event.joins(:attendees).where(attendees: { user_id: current_user.id }).where("date < ?", Time.now).where.not(creator_id: current_user.id).page(params[:page]).per(8)
     end
   end
- 
+
   private
-  
+
    def user_params
     params.require(:user).permit(:name, :introduction, :image, :post_status)
    end
-   
+
     def ensure_guest_user #ゲストユーザーログイン
        @user = User.find(params[:id])
         if @user.guest_user?
           redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
         end
-    end  
+    end
 
- 
+
 end
