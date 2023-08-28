745538faaa7a2402cca076ad8d3a961610f28ac2
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-28 04:42:27 +0000

    pull

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,7 +5,7 @@ class HomesController < ApplicationController
     # いいねが多い順に表示する
     @favorites = Post.find(Favorite.group(:post_id).order('count(post_id) DESC').limit(6).pluck(:post_id))
     # 閲覧数が多い順
-    @impression = Post.impressions_count.limit(6)
+    @impressions = Post.impressions_count.limit(6)
     
     @events = Event.all
     
