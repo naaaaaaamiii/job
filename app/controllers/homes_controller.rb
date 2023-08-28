e45db5d0057c8b2bbcabf5596b472d238e4cd9d4
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-28 05:19:45 +0000

    pull

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,11 @@
 class HomesController < ApplicationController
   def top
     #新着投稿順に表示する
-    @posts = Post.latest.limit(6)
+    @posts = Post.published.latest.limit(6)
     # いいねが多い順に表示する
     @favorites = Post.find(Favorite.group(:post_id).order('count(post_id) DESC').limit(6).pluck(:post_id))
     # 閲覧数が多い順
-    @impressions = Post.impressions_count.limit(6)
+    @rank_posts = Post.order(impressions_count: 'DESC').limit(6) 
     
     @events = Event.all
     
