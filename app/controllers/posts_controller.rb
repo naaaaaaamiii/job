e2b7ce4a95e77d13a7db953c7f4b9f09105c5001
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-29 13:24:08 +0000

    about layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,13 +66,13 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :post_status, :post_image)
   end
-  
+
   def ensure_correct_user
     @post = Post.find(params[:id])
     unless @post.user == current_user
       redirect_to root_path
     end
   end
-  
-  
+
+
 end
