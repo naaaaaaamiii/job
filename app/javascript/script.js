71e187f70d67570989e1d34e8017f07b0fde3f96
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-29 07:30:16 +0000

    layout about

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -21,3 +21,26 @@ $('.nav_toggle').on('click', function () {
   $('.nav_toggle, .nav').toggleClass('show');
 });
 
+// 文字が動くアニメーション
+　function slideAnime(){
+    $('.leftAnime').each(function(){ 
+      var elemPos = $(this).offset().top-50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+        $(this).addClass("slideAnimeLeftRight"); 
+        $(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");
+      }else{
+        $(this).removeClass("slideAnimeLeftRight");
+        $(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+      }
+    });
+  }
+ 
+  $(window).scroll(function (){
+    slideAnime();
+  });
+   $(window).on('load', function(){
+    slideAnime();/* アニメーション用の関数を呼ぶ*/
+  });
+  
