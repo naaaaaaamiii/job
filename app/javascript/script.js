7f7afe97ba4bf56f0b47251df595dc3e79e84d20
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-08-30 01:40:11 +0000

    about layout

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -48,22 +48,4 @@ $('.nav_toggle').on('click', function () {
   
   
   // じわっとでてくる
-  function BlurTextAnimeControl() {
-  $('.blurTrigger').each(function(){ 
-    var elemPos = $(this).offset().top-50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('blur');
-    }else{
-    $(this).removeClass('blur');
-    }
-    });
-}
-
-$(window).scroll(function () {
-  BlurTextAnimeControl();
-});
-$(window).on('load', function () {
-  BlurTextAnimeControl();
-});
+  
