21d090b6d9d76b219179ab201887077872712005
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-09-09 13:44:18 +0000

    tag search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,10 +2,10 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     @word = params[:word]
-    if @range == "User"
-      @users = User.looks(params[:search], params[:word])
+    if @range == "tag"
+      @tags = PostTag.where(params[:search], params[:word])
     else
-      @posts = Post.looks(params[:search], params[:word])
+      @posts = Post.published.looks(params[:search], params[:word])
     end
   end
 end
