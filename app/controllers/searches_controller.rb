bf9f14b18ec757580429cf9914a3bdf62546e9c5
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-09-09 14:10:52 +0000

    tag search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,10 +2,8 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     @word = params[:word]
-    if @range == "tag"
-      @tags = PostTag.where(params[:search], params[:word])
-    else
-      @posts = Post.published.looks(params[:search], params[:word])
-    end
+    @model = Post
+    @posts = Post.published.where("category LIKE?","%#{@word}%")
+    @posts = Post.published.looks(params[:search], params[:word])
   end
 end
