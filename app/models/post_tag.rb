bb1e8a22bd958239b4f0ae5bbab43a3c379ce9ee
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-09-12 02:14:24 +0000

    posttag

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -3,13 +3,13 @@ class PostTag < ApplicationRecord
   has_many :post_tag_relationships, dependent: :destroy
   
    def self.search_content(content, method)
-    if method == "perfect"
-      where(name: content)
-    elsif method == "partial"
-      where("name LIKE ?", "%#{content}%")
-    else
-      all
-    end
+      if method == "perfect"
+        where(name: content)
+      elsif method == "partial"
+        where("name LIKE ?", "%#{content}%")
+      else
+        all
+      end
    end
 end
 
